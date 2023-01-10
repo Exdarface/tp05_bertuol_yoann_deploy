68ff560d26ce7233755f14d99e72f3fa9f41c8b2
Yoann Bertuol <exdarface@gmail.com>
2023-01-10 23:24:07 +0100

    try

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -7,14 +7,16 @@ use Doctrine\ORM\EntityManager;
 
 date_default_timezone_set('America/Lima');
 
+
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'dpg-ceurj2g2i3mq2ss975d0-a',
-'driver' => 'pdo_pgsql',
-'user' => 'yb_db_user',
-'password' => 'A7CJZuDE72Q9of3lovUK2xEB90ySx0Zq',
-'dbname' => 'yb_db',
-'port' => '5432'
+	'host' => 'dpg-ceurj2g2i3mq2ss975d0-a.frankfurt-postgres.render.com',
+	'driver' => 'pdo_pgsql',
+	'user' => 'yb_db_user',
+	'password' => 'A7CJZuDE72Q9of3lovUK2xEB90ySx0Zq',
+	'dbname' => 'yb_db',
+	'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
+
